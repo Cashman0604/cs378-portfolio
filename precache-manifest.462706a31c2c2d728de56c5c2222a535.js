e0e1d58c018343766776f18a12774fe728463556
Cash Belknap <belknapct0604@gmail.com>
2024-02-09 15:56:42 -0600

    Deploy React app to GitHub Pages

---
+++ precache-manifest.462706a31c2c2d728de56c5c2222a535.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1627fec669228bef9462f2003c0ce95d",
+    "url": "/cs378-portfolio/public/index.html/index.html"
+  },
+  {
+    "revision": "d636fc9733bd74a7a4b4",
+    "url": "/cs378-portfolio/public/index.html/static/css/2.cc82f8aa.chunk.css"
+  },
+  {
+    "revision": "d2eb4f20ea11b498c0d5",
+    "url": "/cs378-portfolio/public/index.html/static/css/main.d472e380.chunk.css"
+  },
+  {
+    "revision": "d636fc9733bd74a7a4b4",
+    "url": "/cs378-portfolio/public/index.html/static/js/2.aac0fe50.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/cs378-portfolio/public/index.html/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "d2eb4f20ea11b498c0d5",
+    "url": "/cs378-portfolio/public/index.html/static/js/main.9c704b27.chunk.js"
+  },
+  {
+    "revision": "e726b6100650af9f5de5",
+    "url": "/cs378-portfolio/public/index.html/static/js/runtime~main.cb0305c8.js"
+  }
+]);
